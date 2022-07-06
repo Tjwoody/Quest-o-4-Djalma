e8623cc7cba25db54e811915a5cbe14115e4f4a7
Tjwoody <105826710+Tjwoody@users.noreply.github.com>
2022-07-06 14:29:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_4_numeros_de_1_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros;
+            for (numeros = 1; numeros <= 50; numeros++)
+            {
+                numeros = numeros ;
+                Console.WriteLine(numeros);
+            }
+            Console.ReadKey();
+        }
+    }
+}
